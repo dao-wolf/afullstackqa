505daaa29741d845adf55ae9babd3af5abc19889
opantsjoha <opantsjoha@gmail.com>
2018-01-26 00:16:18 +0000

    moved nav list to app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 (function(){
-  // set up body
-  var nav = document.getElementById('navbar-expand-sm-dark')
-  nav.className = 'navbar navbar-expand-sm bg-dark navbar-dark'
+  function createNav() {
+    // set up body
+    var nav = document.getElementById('navbar-expand-sm-dark');
+    nav.className = 'navbar navbar-expand-sm bg-dark navbar-dark';
+    function createNavList() {
+      var listOfLinks = ['JS','Swift','Java','C#'];
+      var navList = document.getElementById('navbar-list');
+      navList.className = 'navbar-nav';
+
+      listOfLinks.forEach(function (item) {
+        var li = document.createElement('LI');
+        li.className = 'nav-item';
+        var alink = document.createElement('A');
+        alink.className = 'nav-link';
+        alink.href = '#';
+        var menuOpt = document.createTextNode(item);
+        navList.appendChild(li).appendChild(alink).appendChild(menuOpt);
+      })
+
+    }
+    createNavList();
+  }
+  createNav();
 })();

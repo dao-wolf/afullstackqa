0e70650446992e025a1f291077f80361de398bac
opantsjoha <opantsjoha@gmail.com>
2018-02-04 23:22:51 +0000

    restructure, following a better ejs tutorial

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const express = require('express')
-const app = express()
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
 
-app.set('view engine', 'ejs')
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// use res.render to load up an ejs view file
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
